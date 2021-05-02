f3c9de7b9f513d87a01ddc017684d20d32ee3ea3
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-05-02 23:15:14 +0100

    delay variable

--- main.ts
+++ main.ts
@@ -1,19 +1,17 @@
+let Delay = 50
 scrollbit.clear()
 scrollbit.show()
-scrollbit.setIcon(
-IconNames.Triangle,
-0,
-0,
-128
-)
-scrollbit.show()
+basic.pause(200)
+scrollbit.scrollText("Hello Big Clive!!", 128, Delay)
 basic.pause(500)
-scrollbit.clear()
-scrollbit.show()
+scrollbit.scrollText("Hello Technical Family!!", 128, Delay)
+basic.pause(500)
 scrollbit.setAll(64)
 scrollbit.show()
+basic.pause(1000)
 basic.forever(function () {
-    scrollbit.setPixel(randint(0, 16), randint(0, 6), randint(48, 64))
+    scrollbit.setPixel(randint(0, 16), randint(0, 6), randint(48, 81))
+    scrollbit.show()
     scrollbit.setPixel(randint(0, 16), randint(0, 6), 0)
     scrollbit.show()
 })
